fd9a052b4f79c09f987a10d06d9063d7c0f6250f
xujing0504 <jingxu0504@gmail.com>
2023-01-03 23:19:33 +0100

    add frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,78 @@
+import axios from "axios";
+import { useEffect, useMemo, useState } from "react";
+import BarChart from "./components/BarChart";
+import PieChart from "./components/PieChart";
+import { Form, Button } from "react-bootstrap";
+
+const DATA_SOURCE = {
+  LANGUAGE: 1,
+  LICENSE: 2,
+};
+const CHART_TYPE = {
+  BAR: 1,
+  PIE: 2,
+};
+
+function App() {
+  const [data, setData] = useState([]);
+  const [chartType, setChartType] = useState(CHART_TYPE.BAR);
+  const [dataScource, setDataScource] = useState(DATA_SOURCE.LANGUAGE);
+
+  useEffect(() => {
+    if (dataScource === DATA_SOURCE.LANGUAGE) {
+      axios
+        .get(`${process.env.REACT_APP_SERVER_URL}/languages`)
+        .then(({ data }) => {
+          const trans = [];
+          data.result.map((item) =>
+            trans.push({ title: item.LANGUAGE, value: item.total_bytes })
+          );
+          setData(trans);
+        });
+    } else {
+      axios
+        .get(`${process.env.REACT_APP_SERVER_URL}/licenses`)
+        .then(({ data }) => {
+          const trans = [];
+          data.result.map((item) =>
+            trans.push({ title: item.license, value: item.total })
+          );
+          setData(trans);
+        });
+    }
+  }, [dataScource]);
+
+  return (
+    <div className="App">
+      <Form.Group controlId="Chart">
+        <Form.Label>Select Chart Type</Form.Label>
+        <Form.Control
+          as="select"
+          value={chartType}
+          onChange={(e) => setChartType(e.target.value)}
+        >
+          <option>choose chart type</option>
+          <option value={CHART_TYPE.BAR}>Bar Chart</option>
+          <option value={CHART_TYPE.PIE}>Pie Chart</option>
+        </Form.Control>
+      </Form.Group>
+      <Form.Group controlId="datasources">
+        <Form.Label>Select Data Type</Form.Label>
+        <Form.Control
+          as="select"
+          value={dataScource}
+          onChange={(e) => setDataScource(e.target.value)}
+        >
+          <option>choose a source</option>
+          <option value={DATA_SOURCE.LANGUAGE}>languages</option>
+          <option value={DATA_SOURCE.LICENSE}>licences</option>
+        </Form.Control>
+      </Form.Group>
+
+      {chartType == CHART_TYPE.BAR && <BarChart data={data} />}
+      {chartType == CHART_TYPE.PIE && <PieChart data={data} />}
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/BarChart.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Chart from "chart.js/auto";
+import { Bar } from "react-chartjs-2";
+
+const BarChart = ({ data }) => {
+  const barData = {
+    labels: data.map(({ title }) => title),
+    datasets: [
+      {
+        data: data.map(({ value }) => value),
+        label: "Infected",
+        backgroundColor: [
+          "#007D9C",
+          "#244D70",
+          "#D123B3",
+          "#F7E018",
+          "#fff",
+          "#FE452A",
+        ],
+        borderColor: [
+          "rgba(255,99,132,1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+        fill: true,
+      },
+    ],
+  };
+
+  return (
+    <div style={{ width: 650, textAlign: "center" }}>
+      <Bar data={barData} width={50} height={50} />
+    </div>
+  );
+};
+
+export default BarChart;
